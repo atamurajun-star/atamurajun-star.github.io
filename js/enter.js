40071cbefcecbfd2fb75e78ae8ed0bb126679098
atamurajun-star <atamurajun@gmail.com>
2026-08-03 18:34:33 +0900

    Update enter.js

--- js/enter.js
+++ js/enter.js
@@ -1,32 +1,36 @@
 /* =========================================
    JUN ATAMURA ENTER
 ========================================= */
-
 const enterScreen = document.getElementById("enterScreen");
-const enterButton = document.getElementById("enterButton");
+
 
 // ENTER画面表示中はスクロール禁止
 document.body.classList.add("enter-locked");
-/* =========================================
-   AMBIENT AUDIO
-========================================= */
+
+
+// =========================================
+// AMBIENT AUDIO
+// =========================================
 
 const ambient = new Audio("music/ambient.mp3");
 
 ambient.loop = true;
-
 ambient.volume = 0.5;
 
 
-/* =========================================
-   ENTER
-========================================= */
-enterButton.addEventListener("click", () => {
+// =========================================
+// ENTER
+// =========================================
 
+enterScreen.addEventListener("click", () => {
+
+  // Ambient music start
   ambient.play();
 
+  // ENTER画面をフェードアウト
   enterScreen.classList.add("hide");
 
+  // フェードアウト完了後にスクロール解除
   setTimeout(() => {
     document.body.classList.remove("enter-locked");
   }, 1500);
